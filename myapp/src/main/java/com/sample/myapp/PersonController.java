fe48beca0d2767a8f4fc70d1c7e27e6a165b9d51
jin <jli899@yaho>
2018-03-31 15:52:32 -0700

    initial message value

--- myapp/src/main/java/com/sample/myapp/PersonController.java
+++ myapp/src/main/java/com/sample/myapp/PersonController.java
@@ -2,9 +2,11 @@ package com.sample.myapp;
 
 public class PersonController {
 
+	private final String message;
 	@RestController
 	public PersonController() {
 		// TODO Auto-generated constructor stub
+		message =  "Hellow world!";
 	}
 
 }
